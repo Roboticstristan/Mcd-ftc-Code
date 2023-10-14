dc8c6af2ddbcbf0fff5a09fce5c87610f1075182
tristanbrewer <tmbrewer@mcdonogh.org>
2023-10-14 12:17:24 -0400

    Intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="intakeTest", group="Linear Opmode")
+public class Intake extends LinearOpMode {
+    private DcMotor motor1 = null;
+    private DcMotor motor2 = null;
+    //2 different directoins
+    public void runOpMode() {
+
+    }
+}
