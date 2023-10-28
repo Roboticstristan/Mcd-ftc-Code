4d5f0662c03822a5e7cbd98a90b4d73e6333ef0b
duncansciubba <dmsciubba@gmail.com>
2023-10-28 10:54:42 -0400

    Testing Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMethods.java
@@ -0,0 +1,293 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class AutoMethods extends LinearOpMode {
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontRight = null;
+    private DcMotor frontLeft = null;
+    private DcMotor backRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor linearSlide = null;
+    //declare color sensor
+    private NormalizedColorSensor colorSensor = null;
+    //private DcMotor colorSensor = null;
+    public Servo claw = null;
+
+    public AutoMethods(DcMotor fL, DcMotor fR, DcMotor bL, DcMotor bR){
+        frontLeft = fL;
+        frontRight = fR;
+        backLeft = bL;
+        backRight = bR;
+    }
+
+    public void setDirectionForward() {
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void setDirectionBackward() {
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+
+    public void resetEncoders() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void STOP_AND_RESET_ENCODERS_ALL_WHEELS() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void SET_POWER_ALL_WHEELS(double power) {
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+    }
+
+    public void DRIVE_DISTANCE_FORWARD(float distance_in_in, double power) {
+        float ticksPerInch = 59.6031746032f;
+        float f_ticks = ticksPerInch * distance_in_in;
+        int ticks = Math.round(f_ticks);
+        // 751.8 ticks per rotation
+
+        if (power > 0) {
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+            frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        } else {
+            backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        STOP_AND_RESET_ENCODERS_ALL_WHEELS();
+        SET_POWER_ALL_WHEELS(power);
+
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        telemetry.addData("motors", "> Are running to position");
+        telemetry.update();
+        while (frontLeft.getCurrentPosition() <= (frontLeft.getTargetPosition() - 50)) {
+            //Wait until job is finished
+            telemetry.addData("motors", "> Are strafing to position");
+            telemetry.addData("ticks", ">" + frontLeft.getCurrentPosition() + " need to get to " + frontLeft.getTargetPosition());
+            telemetry.update();
+        }
+    }
+
+    void TURN(int power, float distance_in_in) {
+        float ticksPerInch = 59.6031746032f;
+        float f_ticks = ticksPerInch * distance_in_in;
+        int ticks = Math.round(f_ticks);
+        // 1120 Ticks per revolution
+
+        if (power > 0) {
+            backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+            frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        } else {
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+
+        //backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        //frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        //frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        //backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        STOP_AND_RESET_ENCODERS_ALL_WHEELS();
+        SET_POWER_ALL_WHEELS(0.6);
+
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        telemetry.addData("motors", "> Are running to position");
+        telemetry.update();
+        while (frontLeft.getCurrentPosition() <= (frontLeft.getTargetPosition() - 50)) {
+            //Wait until job is finished
+            telemetry.addData("motors", "> Are strafing to position");
+            telemetry.addData("ticks", ">" + frontLeft.getCurrentPosition() + " need to get to " + frontLeft.getTargetPosition());
+            telemetry.update();
+        }
+
+        STOP_AND_RESET_ENCODERS_ALL_WHEELS();
+
+        telemetry.addData("motors", "> have run to position");
+        telemetry.update();
+    }
+
+    public void DRIVE_DISTANCE_RIGHT(float distance_in_in) {
+        float ticksPerInch = 120.737061895f;
+        float f_ticks = ticksPerInch * distance_in_in;
+        int ticks = Math.round(f_ticks);
+        // 1120 Ticks per revolution
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        STOP_AND_RESET_ENCODERS_ALL_WHEELS();
+        SET_POWER_ALL_WHEELS(1);
+
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        telemetry.addData("motors", "> Are running to position");
+        telemetry.update();
+        while (frontLeft.isBusy()) {
+            //Wait until job is finished
+        }
+
+        STOP_AND_RESET_ENCODERS_ALL_WHEELS();
+
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("motors", "> have run to position");
+        telemetry.update();
+    }
+
+    public void DRIVE_DISTANCE_LEFT(float distance_in_in) {
+        float ticksPerInch = 120.737061895f;
+        float f_ticks = ticksPerInch * distance_in_in;
+        int ticks = Math.round(f_ticks);
+        // 1120 Ticks per revolution
+
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        STOP_AND_RESET_ENCODERS_ALL_WHEELS();
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        SET_POWER_ALL_WHEELS(1);
+
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        telemetry.addData("motors", "> Are strafing to position");
+        telemetry.update();
+        while (frontLeft.getCurrentPosition() <= (frontLeft.getTargetPosition() - 50)) {
+            //Wait until job is finished
+            telemetry.addData("motors", "> Are strafing to position");
+            telemetry.addData("ticks", ">" + frontLeft.getCurrentPosition());
+            telemetry.update();
+        }
+
+    }
+
+    public void LINEAR_SLIDE_DRIVE(float distance_in_in, double power) {
+        float ticksPerInch = 450.149432158f;
+        float f_ticks = ticksPerInch * distance_in_in;
+        int ticks = Math.round(f_ticks);
+        //753.1 ticks per revolution
+        //1.673 in per revolution (circumference)
+        //450.149432158 ticks per in
+        if (power > 0) {
+            //go up
+            linearSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        } else {
+            //go down
+            linearSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearSlide.setPower(power);
+        linearSlide.setTargetPosition(ticks);
+        linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        telemetry.addData("slide", "> is running to position");
+        telemetry.update();
+        while (linearSlide.getCurrentPosition() <= (linearSlide.getTargetPosition() - 50)) {
+            //Wait until job is finished
+            telemetry.addData("slide", "> is strafing to position");
+            telemetry.addData("ticks", ">" + linearSlide.getCurrentPosition() + " need to get to " + linearSlide.getTargetPosition());
+            telemetry.update();
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+    }
+}
