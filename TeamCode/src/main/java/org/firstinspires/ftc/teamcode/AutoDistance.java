ccfe3148b10216a72beea68285e2210851a0f7bb
tristanbrewer <tmbrewer@mcdonogh.org>
2023-10-27 21:06:20 -0400

    linearslide fixed,distance has been worked on

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDistance.java
@@ -0,0 +1,54 @@
+/**
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name ="Autoshot", group ="Auto2023")
+
+
+public class AutoDistance extends Autonomous{
+    private DistanceSensor sensorRange1;
+    private DistanceSensor sensorRange2;
+    private ElapsedTime runtime = new ElapsedTime();
+    @Override
+
+    public void distanceInput(){
+        sensorRange1 = hardwareMap.get(DistanceSensor.class, "Distance1");
+        sensorRange2 = hardwareMap.get(DistanceSensor.class, "Distance2");
+
+        double m1 = sensorRange1.getDistance(DistanceUnit.CM);
+        double m2 = sensorRange2.getDistance(DistanceUnit.CM);
+
+        if(m1 > m2 && m1 > 0.5 && m2 > 0.5){
+            return 1;
+        } else if(m2 > m1 && m1 > 0.5 && m2 > 0.5){
+            return 2;
+        } else {
+            return 3;
+        }
+
+
+    }
+
+    public void runOpMode() {
+        // you can use this as a regular DistanceSensor.
+
+        waitForStart();
+        while(opModeIsActive()) {
+
+        }
+
+    }
+}
+**/
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linslide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linslide.java
@@ -8,24 +8,32 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp(name="linslide", group="Linear Opmode")
 public class linslide extends LinearOpMode {
     private DcMotor linearSlide = null;
+    private DcMotor linearSlide1 = null;
     private ElapsedTime runtime = new ElapsedTime();
         public void runOpMode() {
             linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
+            linearSlide1 = hardwareMap.get(DcMotor.class, "linear_slide2");
             waitForStart();
             runtime.reset();
             while (opModeIsActive()){
                 if(gamepad1.left_trigger > 0)
             {
                 linearSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+                linearSlide1.setDirection(DcMotorSimple.Direction.FORWARD);
                 linearSlide.setPower(0.6);
+                linearSlide1.setPower(0.6);
             }else if(gamepad1.right_trigger > 0)
                 {
                 linearSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+                linearSlide1.setDirection(DcMotorSimple.Direction.REVERSE);
                 linearSlide.setPower(0.9);
+                linearSlide1.setPower(0.9);
             } else {
                     {
                         linearSlide.setDirection(DcMotorSimple.Direction.FORWARD);
-                        linearSlide.setPower(0.05);
+                        linearSlide1.setDirection(DcMotorSimple.Direction.FORWARD);
+                        linearSlide.setPower(0.09);
+                        linearSlide1.setPower(0.09);
                     }
                 }
             }
