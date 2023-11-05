b3511f88deb55233281f594d88c413920cd17721
tristanbrewer <tmbrewer@mcdonogh.org>
2023-11-05 16:07:35 -0500

    auto work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onlyonelinslide.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="0nlyonelineslide", group="Linear Opmode")
+public class onlyonelinslide extends LinearOpMode
+{
+    private DcMotor linearSlide = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    public void runOpMode()
+    {
+        linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
+        waitForStart();
+        runtime.reset();
+        while (opModeIsActive())
+        {
+            if(gamepad1.left_trigger > 0)
+            {
+                linearSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+                linearSlide.setPower(0.6);
+
+            }else if(gamepad1.right_trigger > 0){
+
+                linearSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+                linearSlide.setPower(0.9);
+
+            } else {
+                    linearSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+                    linearSlide.setPower(0.09);
+            }
+        }
+    }
+}
