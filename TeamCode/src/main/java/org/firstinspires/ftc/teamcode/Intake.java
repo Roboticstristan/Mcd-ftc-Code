90eadb640c497a5df036b54e3e93d1da27477e21
tristanbrewer <tmbrewer@mcdonogh.org>
2023-11-20 16:44:11 -0500

    Changing names bc coaches were complaining

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -9,33 +9,33 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="intakeTest", group="Linear Opmode")
-public class Intake extends LinearOpMode {
+public class Intake extends LinearOpMode
+{
     private DcMotor motor1 = null;
     //private DcMotor motor2 = null;
     private ElapsedTime runtime = new ElapsedTime();
-    public void runOpMode() {
+    public void runOpMode()
+    {
        int count = 0;
         waitForStart();
         runtime.reset();
-        while (opModeIsActive()) {
+        while (opModeIsActive())
+        {
             motor1 = hardwareMap.get(DcMotor.class, "mouthI");
             //motor2 = hardwareMap.get(DcMotor.class, "bodyI");
             motor1.setDirection(DcMotorSimple.Direction.FORWARD);
-            if(gamepad1.x){
+            if(gamepad1.x)
+            {
                 count++;
                 if (count % 2 != 0) {
                     motor1.setPower(1);
                     //motor2.setDirection(DcMotorSimple.Direction.REVERSE);
                     //motor2.setPower(1);
                 } else {
-                    motor1.setPower(-0.5);
+                   motor1.setPower(0);
                 }
+
             }
-            if(gamepad1.a){
-                count = 0;
-                motor1.setPower(0);
-            }
-        }
 
     }
 }
