1aa5455e1d252c5cc23ce5070a2d33a45a9a55d6
tristanbrewer <tmbrewer@mcdonogh.org>
2024-01-19 14:50:04 -0500

    Fixing auto and moving it over to red side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GavinoAutoIIRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GavinoAutoIIRed.java
@@ -424,8 +424,10 @@ public class GavinoAutoIIRed extends LinearOpMode {
         sleep(500);
         DRIVE_DISTANCE_LEFT(28f);
         sleep(500);
-        DRIVE_DISTANCE_FORWARD(4,1);
-        sleep(500);
+        if(pplace == 0){
+            DRIVE_DISTANCE_FORWARD(4,1);
+            sleep(500);
+        }
         TURN(1,20);
         //Add linear slide
         sleep(100);
